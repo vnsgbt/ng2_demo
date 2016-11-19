2451ddb51b911f0b3342cadfba0ec414b3a21dc5
steve <vnsgbt@me.com>
2016-11-19 11:16:45 -0600

    multi components and dist

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,24 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero'
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
 
-    <h2>My Heroes</h2>    
+      <h2>My Heroes</h2>    
+      
       <ul class="heroes">
         <li *ngFor="let hero of heroes"
           [class.selected]="hero === selectedHero"
           (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
-      </ul>`,
+      </ul>
+    
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    
+    `,
 
   styles: [`
   .selected {
@@ -81,11 +78,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number
-  name: string
 }
 
 let HEROES: Hero[] = [
