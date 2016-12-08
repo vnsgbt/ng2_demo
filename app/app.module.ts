723f295df3a95d2e5156c01644354544a326d7dc
steve <vnsgbt@me.com>
2016-12-08 15:22:23 -0600

    route config

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule }   from '@angular/forms'
 import { AppComponent } from './app.component'
 import { HeroDetailComponent } from './heroDetail.component'
+import { HeroesComponent } from './heroes.component'
+import { RouterModule } from '@angular/router'
+import { HeroService } from './hero.service'
+
+RouterModule.forRoot([
+  {
+    path: "heroes",
+    component: HeroesComponent
+  }
+])
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [AppComponent, HeroDetailComponent],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule ,
+    RouterModule.forRoot([
+      {
+        path: "heroes",
+        component: HeroesComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent
+    ],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [AppComponent]
 })
 
